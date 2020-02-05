50f766296abc79db4f4e535d8a093af659b626a9
KaranSinghG <karansinghg@ymail.com>
2020-02-05 16:29:40 +0530

    Todo Item frontend

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,6 +2,19 @@ import React, { Component } from "react";
 
 export default class extends Component {
   render() {
-    return <div>item</div>;
+    const { title, handleDelete, handleEdit } = this.props;
+    return (
+      <li className="list-group-item d-flex text-capitalize justify-content-between my-0">
+        <h6>{title}</h6>
+        <div className="todo-icon">
+          <span className="text-success mx-2" onClick={handleEdit}>
+            <i className="fas fa-pen"></i>
+          </span>
+          <span className="text-danger mx-2" onClick={handleDelete}>
+            <i className="fas fa-trash"></i>
+          </span>
+        </div>
+      </li>
+    );
   }
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
 import Item from "./TodoItem.js";
+import TodoItem from "./TodoItem.js";
 
 export default class TodoList extends Component {
   render() {
+    const { items, clearList, handleDelete, handleEdit } = this.props;
     return (
-      <div>
-        <h2>Todo List</h2>
-        <Item></Item>
-        <Item></Item>
-      </div>
+      <ul className="list-group my-5">
+        <h3 className="text-capitalize text-center">Todo List</h3>
+        {items.map(item => {
+          return (
+            <TodoItem
+              key={item.id}
+              title={item.title}
+              handleDelete={() => handleDelete(item.id)}
+              handleEdit={() => handleEdit(item.id)}
+            ></TodoItem>
+          );
+        })}
+        <button
+          type="button"
+          className="btn btn-danger btn-block text-uppercase mt-5"
+          onClick={clearList}
+        >
+          Clear list
+        </button>
+      </ul>
     );
   }
 }
